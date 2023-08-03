fb86366bafc2a303a1a03d31715cfd0bc98557c6
Jensy Figueroa <jensy1418@gmail.com>
2023-08-03 19:35:27 -0400

    Se le da funcionalidad el boton del Dark

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import IcoMoon from "../../icons/IcoMoon"
 import IcoSun from '../../icons/IcoSun'
 
 const Header = () => {
 
-    const [darkMode, setDarkMode] = useState(false)
+    const [darkMode, setDarkMode] = useState(true)
 
     const handleChangeToogleTheme = () => {
         setDarkMode(!darkMode)
     }
+
+    useEffect(() => { 
+        if (darkMode) {
+            document.documentElement.classList.add('dark')
+        }
+        else{
+            document.documentElement.classList.remove('dark')
+        }
+     },[darkMode])
 
     return (
         <header className="container mx-auto px-4  pt-8">
@@ -16,7 +25,7 @@ const Header = () => {
                 <h1 className="uppercase text-white text-3xl font-semibold tracking-[0.3em]">Todo</h1>
                 <button onClick={handleChangeToogleTheme}>
                     {
-                        darkMode ? <IcoSun/> : <IcoMoon className="fill-black-400" />
+                        darkMode ? <IcoSun/> : <IcoMoon className="fill-black-400" /> 
                     }
                     
                 </button>
