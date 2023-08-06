e2034a3f850fe2b6fc680826c143171ce1ca9e21
Jensy Figueroa <jensy1418@gmail.com>
2023-08-06 09:58:12 -0400

    Se deja como predeterminado ALL en el filter

--- src/components/filterTodo/FilterTodo.jsx
+++ src/components/filterTodo/FilterTodo.jsx
@@ -6,11 +6,13 @@ const FilterTodo = () => {
 
     const dispatch = useDispatch()
 
-    const [filter, setFilter] = useState()
+    const [filter, setFilter] = useState('ALL')
+
+    console.log(filter,'filter')
 
     return (
         <section className="container mx-auto mt-8 px-4">
-            <div className='bg-white rounded-md flex justify-evenly p-4 dark:bg-gray-800 transition-all duration-1000'>
+            <div className='bg-white rounded-md flex justify-evenly p-4 dark:bg-gray-800 '>
                 <button className={`${filter === 'ALL' && 'text-blue-600'} hover:text-blue-600`} onClick={() => {dispatch(filterTodos('ALL')), setFilter('ALL')}}>All</button>
                 <button className={`${filter === 'ACTIVE' && 'text-blue-600'} hover:text-blue-600`} onClick={() => {dispatch(filterTodos('ACTIVE')), setFilter('ACTIVE')}}>Active</button>
                 <button className={`${filter === 'COMPLETED' && 'text-blue-600'} hover:text-blue-600`} onClick={() => {dispatch(filterTodos('COMPLETED')), setFilter('COMPLETED')}}>Completed</button>
